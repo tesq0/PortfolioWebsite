79ffad708da43fdf5490709483c49ff150f99bfc
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2020-11-19 00:03:13 +0100

    js change tab width

--- src/themes/mikus-blog/assets/js/matrix-effect.js
+++ src/themes/mikus-blog/assets/js/matrix-effect.js
@@ -3,7 +3,7 @@ const COLUMN_PX_WIDTH = 20;
 const ROW_PX_HEIGHT = 20;
 const MOVE_SPEED = 50;
 
-const CHINESE_CHARACTERS = ['中','文','简','繁','体','转','换','器','-','支','持','地','方','惯','用','词','汇','替','换'];
+const CHINESE_CHARACTERS = ['中', '文', '简', '繁', '体', '转', '换', '器', '-', '支', '持', '地', '方', '惯', '用', '词', '汇', '替', '换'];
 
 var currentMaxRows = 0;
 var currentMaxOffset = 0;
@@ -18,167 +18,167 @@ const columnMatrixes = new Map();
 
 class ColumnMatrix {
 
-	constructor(rowOffset, characters) {
+  constructor(rowOffset, characters) {
 
-		this.rowOffset = rowOffset;
-		this.characters = characters;
+    this.rowOffset = rowOffset;
+    this.characters = characters;
 
-		this.move = this.move.bind(this);
-		this.getData = this.getData.bind(this);
-		
-	}
+    this.move = this.move.bind(this);
+    this.getData = this.getData.bind(this);
 
-	static make() {
+  }
 
-		const characters = [];
-		
-		const characterCount = Math.max(
-			2,
-			Math.floor( Math.random() * (currentMaxRows / 2) )
-		);
+  static make() {
 
-		const rowOffset = Math.max(
-			2,
-			Math.floor( Math.random() * ( currentMaxRows - characterCount ) )
-		) * ROW_PX_HEIGHT;
-		
-		for (let i = 0; i < characterCount; i++) {
+    const characters = [];
 
-			let characterIdx = Math.floor(Math.random() * CHINESE_CHARACTERS.length);
-			let character = CHINESE_CHARACTERS[characterIdx];
-			characters.push(character);
-			
-		}
+    const characterCount = Math.max(
+      2,
+      Math.floor(Math.random() * (currentMaxRows / 2))
+    );
 
-		return new ColumnMatrix(rowOffset, characters);
-		
-	}
+    const rowOffset = Math.max(
+      2,
+      Math.floor(Math.random() * (currentMaxRows - characterCount))
+    ) * ROW_PX_HEIGHT;
 
-	move(amount = 1) {
+    for (let i = 0; i < characterCount; i++) {
 
-		/* console.log("move ", amount); */
+      let characterIdx = Math.floor(Math.random() * CHINESE_CHARACTERS.length);
+      let character = CHINESE_CHARACTERS[characterIdx];
+      characters.push(character);
 
-		let newYOffset = this.rowOffset + amount;
-		
-		if ( newYOffset >= (currentMaxOffset + ROW_PX_HEIGHT) ) {
+    }
 
-			newYOffset = (-1 * this.characters.length) * ROW_PX_HEIGHT;
+    return new ColumnMatrix(rowOffset, characters);
 
-		}
+  }
 
-		newYOffset = Math.ceil(newYOffset);
-		
-		this.rowOffset = newYOffset;
-		
-	}
+  move(amount = 1) {
 
-	getData() {
+    /* console.log("move ", amount); */
 
-		return { rowOffset: this.rowOffset, characters: this.characters };
-		
-	}
-	
+    let newYOffset = this.rowOffset + amount;
+
+    if (newYOffset >= (currentMaxOffset + ROW_PX_HEIGHT)) {
+
+      newYOffset = (-1 * this.characters.length) * ROW_PX_HEIGHT;
+
+    }
+
+    newYOffset = Math.ceil(newYOffset);
+
+    this.rowOffset = newYOffset;
+
+  }
+
+  getData() {
+
+    return { rowOffset: this.rowOffset, characters: this.characters };
+
+  }
+
 }
 
 const getOrInitColumnMatrix = (columnIndex) => {
 
-	let key = columnIndex.toString();
+  let key = columnIndex.toString();
 
-	let obj = columnMatrixes.get(key);
+  let obj = columnMatrixes.get(key);
 
-	if (obj == undefined) {
-		obj = ColumnMatrix.make();
-		columnMatrixes.set(key, obj);
-	}
+  if (obj == undefined) {
+    obj = ColumnMatrix.make();
+    columnMatrixes.set(key, obj);
+  }
 
-	return obj;
-	
+  return obj;
+
 }
 
 const drawMatrix = () => {
 
-	let screenWidth = window.innerWidth;
-	let screenHeight = window.innerHeight;
-	
-	const c = document.getElementById("matrix-canvas");
-	const ctx = c.getContext("2d");
+  let screenWidth = window.innerWidth;
+  let screenHeight = window.innerHeight;
 
-	c.width = screenWidth;
-	c.height = screenHeight;
-	
-	// Draw the background
-	let gradient = ctx.createLinearGradient(0, 0, 0, 0.7 * screenHeight);
-	gradient.addColorStop(0, "rgba(0, 0, 0, 1)");
-	gradient.addColorStop(1, "rgba(0, 50, 0, 0.7)");
+  const c = document.getElementById("matrix-canvas");
+  const ctx = c.getContext("2d");
 
-	ctx.fillStyle = gradient;
-	ctx.font = `${ROW_PX_HEIGHT}px Sans`;
-	ctx.fillRect(0, 0, screenWidth, screenHeight);
+  c.width = screenWidth;
+  c.height = screenHeight;
 
-	// Draw the letters
-	let columnCount = screenWidth / COLUMN_PX_WIDTH;
-	let rowCount = screenHeight / ROW_PX_HEIGHT;
+  // Draw the background
+  let gradient = ctx.createLinearGradient(0, 0, 0, 0.7 * screenHeight);
+  gradient.addColorStop(0, "rgba(0, 0, 0, 1)");
+  gradient.addColorStop(1, "rgba(0, 50, 0, 0.7)");
 
-	currentMaxRows = rowCount;
-	currentMaxOffset = rowCount * ROW_PX_HEIGHT;
+  ctx.fillStyle = gradient;
+  ctx.font = `${ROW_PX_HEIGHT}px Sans`;
+  ctx.fillRect(0, 0, screenWidth, screenHeight);
+
+  // Draw the letters
+  let columnCount = screenWidth / COLUMN_PX_WIDTH;
+  let rowCount = screenHeight / ROW_PX_HEIGHT;
+
+  currentMaxRows = rowCount;
+  currentMaxOffset = rowCount * ROW_PX_HEIGHT;
 
 
-	for (let col = 0 ; col < columnCount; col++) {
+  for (let col = 0; col < columnCount; col++) {
 
-		let xPos = col * COLUMN_PX_WIDTH;
+    let xPos = col * COLUMN_PX_WIDTH;
 
-		const columnMatrix = getOrInitColumnMatrix(col);
-		const { rowOffset, characters } = columnMatrix.getData();
+    const columnMatrix = getOrInitColumnMatrix(col);
+    const { rowOffset, characters } = columnMatrix.getData();
 
-		let moveBy = deltaTime * MOVE_SPEED;
-		columnMatrix.move(moveBy);
+    let moveBy = deltaTime * MOVE_SPEED;
+    columnMatrix.move(moveBy);
 
-		for (let charIdx = 0; charIdx < characters.length; charIdx++) {
+    for (let charIdx = 0; charIdx < characters.length; charIdx++) {
 
-			let yPos = rowOffset + (charIdx * ROW_PX_HEIGHT);
+      let yPos = rowOffset + (charIdx * ROW_PX_HEIGHT);
 
-			if (yPos < currentMaxOffset) {
-				ctx.fillStyle=`rgba(0, 255 ,0 , ${1 - (yPos / screenHeight)})`;
-				ctx.fillText(characters[charIdx], xPos, yPos);
-			}
+      if (yPos < currentMaxOffset) {
+        ctx.fillStyle = `rgba(0, 255 ,0 , ${1 - (yPos / screenHeight)})`;
+        ctx.fillText(characters[charIdx], xPos, yPos);
+      }
 
-			/*
-			const padding = 5;
-			const rectWidth = COLUMN_PX_WIDTH - padding;
-			const rectHeight = ROW_PX_HEIGHT - padding;
-			const centerOut = padding / 2;
+      /*
+  const padding = 5;
+  const rectWidth = COLUMN_PX_WIDTH - padding;
+  const rectHeight = ROW_PX_HEIGHT - padding;
+  const centerOut = padding / 2;
 
-			 ctx.fillRect(
-				 xPos + (centerOut),
-				 yPos + (centerOut),
-				 rectWidth , rectHeight
-			 );
-			*/
-			
-		}
-		
-	}
+  ctx.fillRect(
+  xPos + (centerOut),
+  yPos + (centerOut),
+  rectWidth , rectHeight
+  );
+      */
 
-	
+    }
+
+  }
+
+
 };
 
 var requestAnimationFrame = window.requestAnimationFrame ||
-															window.webkitRequestAnimationFrame ||
-															window.mozRequestAnimationFrame ||
-															window.oRequestAnimationFrame ||
-															window.msRequestAnimationFrame;
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.oRequestAnimationFrame ||
+  window.msRequestAnimationFrame;
 
 
 var cancelAnimationFrame = window.cancelAnimationFrame ||
-        window.webkitCancelRequestAnimationFrame || 
-        window.webkitCancelAnimationFrame ||
-        window.mozCancelRequestAnimationFrame || window.mozCancelAnimationFrame ||
-        window.oCancelRequestAnimationFrame || window.oCancelAnimationFrame ||
-        window.msCancelRequestAnimationFrame || window.msCancelAnimationFrame;
+  window.webkitCancelRequestAnimationFrame ||
+  window.webkitCancelAnimationFrame ||
+  window.mozCancelRequestAnimationFrame || window.mozCancelAnimationFrame ||
+  window.oCancelRequestAnimationFrame || window.oCancelAnimationFrame ||
+  window.msCancelRequestAnimationFrame || window.msCancelAnimationFrame;
 
 function draw() {
-	
-	drawMatrix();
+
+  drawMatrix();
 
 }
 
@@ -186,50 +186,50 @@ var activeAnimationFrame = null;
 
 function render() {
 
-	if (!document.hasFocus()) {
-		return
-	}
-	
-	now = performance.now( );
-	deltaTime = ( now - lastTimestamp ) / 1000.0;
-	lastTimestamp = now;
-	draw();
+  if (!document.hasFocus()) {
+    return
+  }
 
-	activeAnimationFrame = requestAnimationFrame(render);
+  now = performance.now();
+  deltaTime = (now - lastTimestamp) / 1000.0;
+  lastTimestamp = now;
+  draw();
+
+  activeAnimationFrame = requestAnimationFrame(render);
 
 }
 
 function cancelAnimation() {
-	if (activeAnimationFrame !== null) {
-		cancelAnimationFrame(activeAnimationFrame);
-	}
+  if (activeAnimationFrame !== null) {
+    cancelAnimationFrame(activeAnimationFrame);
+  }
 }
 
 function animate() {
 
-	cancelAnimation();
-	lastTimestamp = performance.now();
-	activeAnimationFrame = requestAnimationFrame(render);
+  cancelAnimation();
+  lastTimestamp = performance.now();
+  activeAnimationFrame = requestAnimationFrame(render);
 
 }
 
 function trackFocus() {
 
-	window.onblur = function() { isWindowFocused = false; }
-	window.onfocus = function() { isWindowFocused = true; }
-	
+  window.onblur = function () { isWindowFocused = false; }
+  window.onfocus = function () { isWindowFocused = true; }
+
 }
 
 function init() {
 
-	const canvas = document.getElementById("matrix-canvas");
+  const canvas = document.getElementById("matrix-canvas");
 
-	if (canvas != null) {
-		draw(); // first draw;
-		window.addEventListener('focus', animate);
-		animate();
-	}
-	
+  if (canvas != null) {
+    draw(); // first draw;
+    window.addEventListener('focus', animate);
+    animate();
+  }
+
 }
 
 trackFocus();
