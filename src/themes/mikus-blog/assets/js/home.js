763e3c46e4750d7fb3f5a05a0cc2aea00b1d906c
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2020-11-18 23:57:06 +0100

    remove js page content switching

--- src/themes/mikus-blog/assets/js/home.js
+++ src/themes/mikus-blog/assets/js/home.js
@@ -1,46 +1,7 @@
-let showStart = true;
-let showFooter = false;
-
-function throttle(cb, time) {
-  let waiting = false;
-  return function () {
-    if (!waiting) {
-      cb.apply(this, arguments);
-      waiting = true;
-      setTimeout(
-        () => waiting = false,
-        time
-      );
-    }
-  };
-}
-
 function scrollToContent() {
   document.getElementById('content').scrollIntoView({
     behavior: 'smooth'
   });
 }
 
-function onScroll(event) {
-  var el = event.target.scrollingElement;
-  if ((el.scrollTop - (window.innerHeight / 2)) > (window.innerHeight)) {
-    if (showFooter == false) {
-      showStart = false;
-      showFooter = true;
-    }
-  }
-  else {
-    if (showFooter == true) {
-      showStart = true;
-      showFooter = false;
-    }
-  }
-
-  document.getElementById('start').style.display = showStart ? null : "none";
-  document.getElementById('footer').style.display = showFooter ? null : "none";
-
-}
-
 window.scrollToContent = scrollToContent;
-document.addEventListener("scroll", throttle(onScroll, 300));
-document.onload = onScroll;
